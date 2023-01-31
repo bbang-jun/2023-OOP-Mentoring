b3a7a7879f4da407885de8c056e202b87a4d95b0
moonuckjoo <dnjswn1756@naver.com>
2023-01-31 19:01:42 +0900

    Feat: Dynamic Array

--- Wonju/Wonju/Wonju/2-1.cpp
+++ Wonju/Wonju/Wonju/2-1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
-class Student_info{
+class Student_info {
 private:
 	char name[10];
 	int age = 0;
@@ -8,11 +10,21 @@ private:
 	char major[100];
 
 public: // 값을 입력받아서 private에 저장
+	Student_info() {};
 	Student_info(char* name, int age, char* univ, char* major)
+	{
+		name = new char[10];
+		strcpy(this->name, name);
+		univ = new char[100];
+		strcpy(this->university, univ);
+		major = new char[100];
+		strcpy(this->major, major);
+		this->age = age;
+	};
+	bool find(char* name)
 	{
 
 	};
-	bool find(char* name);
 	void print()  //private의 저장된 것 출력
 	{
 		cout << "----------print----------";
@@ -20,28 +32,52 @@ public: // 값을 입력받아서 private에 저장
 		cout << "Age : " << age << endl;
 		cout << "University : " << university << endl;
 		cout << "Major : " << major << endl;
-		cout << "-------------------------"<< endl;
+		cout << "-------------------------" << endl;
 	};
-	void change(char* name, int age, char* univ, char* major); //set함수를 통해서 바꾸기
-};
-int main(void)
-{
-	char input[10]; // 커맨드 저장
-		// 클래스 메소드에 전달할 정보 저장
-	char name1[10];
-	int age1;
-	char univ1[100];
-	char major1[100];
-	int i = 0;
-	Student_info* array[10]; // 정보를 입력할 배열 구현
-	while (1) // 무한 반복
+	void change(char* name, int age, char* univ, char* major)
 	{
-		cout << "Please Enter Command(Insert, find, change, print, exit) : "; 
-		cin >> input; // 커맨드를 입력받음
-		if (input == "insert" && " Insert") // insert인 경우
+
+	};
+	~Student_info() {};
+
+	int main (void)
+	{
+		Student_info* Student[10] = {};
+		char input[10]; // 커맨드 저장
+			// 클래스 메소드에 전달할 정보 저장
+		char name1[10];
+		int age1;
+		char univ1[100];
+		char major1[100];
+		int i = 0;
+		
+		while (1) // 무한 반복
 		{
-			cin >> name1 >> age1 >> univ1 >> major1; // 정보를 모두 입력받음.
-			//array[i].Student_info(name1, age1, univ1, major1); //입력받은 정보를 전달해줄 계획
-			i++; // 다음 사람거
+			cout << "Please Enter Command(Insert, find, change, print, exit) : ";
+			cin >> input; // 커맨드를 입력받음
+			if (input == "insert") // insert인 경우
+			{
+				cin >> name1 >> age1 >> univ1 >> major1; // 정보를 모두 입력받음.
+				Student[i] = new Student_info(name1,age1, univ1, major1);
+				i++; // 다음 사람거
+			}
+			else if (input == "find")
+			{
+
+			}
+			else if (input == "print")
+			{
+
+			}
+			else if (input == "exit")
+			{
+				break;
+			}
+			else
+			{
+				cout << "Please Enter Command(insert, find, change, print, exit) : ";
+				cin >> input; // 커맨드를 입력받음
+			}
 		}
 	}
+	};
