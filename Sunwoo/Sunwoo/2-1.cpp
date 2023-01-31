a2ba6b5d229a70609a9879547fce1ec8702d892a
unknown <davidyeon123@naver.com>
2023-01-31 19:40:15 +0900

    Fix: class의 public insert 함수 수정

--- Sunwoo/Sunwoo/2-1.cpp
+++ Sunwoo/Sunwoo/2-1.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class Student_info
@@ -11,25 +12,25 @@ private:
 	char major[100];
 
 public:
-	Student_info() {};
+	//Student_info() {}; // 기본 생성자
 	Student_info(char* name, int age, char* univ, char* major)
 	{
-		name = new char[10];
 		strcpy(this->name, name);
 		this->age = age;
-		univ = new char[100];
 		strcpy(this->university, univ);
-		major = new char[10];
 		strcpy(this->major, major);
 	};
-	bool find(char* name);
-	void print()
+	bool find(char* name)
 	{
-		cout << "----------print----------" << endl;
+
+	};
+	void print() // 객체에 저장된 정보 한번 출력, main에서 반복 호출
+	{
 		cout << "Name : " << this->name << endl;
+		cout << "Age : " << this->age << endl;
 		cout << "University : " << this->university << endl;
 		cout << "Major : " << this->major << endl;
-		cout << "----------print----------" << endl;
+		cout << "-------------------------" << endl;
 	};
 	void change(char* name, int age, char* univ, char* major);
 };
@@ -37,11 +38,42 @@ public:
 int main(void)
 {
 	char command[10] = "", name[10], univ[100], major[100];
-	int age;
-	Student_info* student = new Student_info[10];
+	int age, cnt = 0;
+	Student_info* student[10];
 
-	cout << "Please Enter Command(insert, find, change, print, exit) :";
-	cin >> command;
+	while (1)
+	{
+		cout << "Please Enter Command(insert, find, change, print, exit) :";
+		cin >> command;
+		if (strcmp(command, "insert") == 0)
+		{
+			cin >> name >> age >> univ >> major;
+			student[cnt] = new Student_info(name, age, univ, major);
+			cnt++;
+		}
+		else if (strcmp(command, "find") == 0)
+		{
+
+		}
+		else if (strcmp(command, "change") == 0)
+		{
+
+		}
+		else if (strcmp(command, "print") == 0)
+		{
+			cout << "----------print----------" << endl;
+			for (int i = 0; i < cnt; i++)
+			{
+				student[i]->print();
+			}
+			cout << "-------------------------" << endl;
+		}
+		else if (strcmp(command, "exit") == 0)
+			return 0;
+		else
+			continue;
+	}
+	
 
 
 
