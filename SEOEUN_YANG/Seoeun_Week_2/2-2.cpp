83db406a961a8d7bb516257d52f795de6cbcd042
SEOEUN YANG <03ann@naver.com>
2023-02-02 23:51:31 +0900

    feat: 2-2 print_all 구현

--- SEOEUN_YANG/Seoeun_Week_2/2-2.cpp
+++ SEOEUN_YANG/Seoeun_Week_2/2-2.cpp
@@ -9,50 +9,53 @@ private:
 	char species[100];
 
 public:
-	
-	animal(char* name, int year, char* species);//자동생성자
+	animal(char* name, int year, char* species);//생성자
+	char* get_name() { return this->name; }
+	int get_year() { return this->year; }
+	char* get_species() { return this->species; }
 };
 
 class zoo {
 private:
-	class animal* animal_list[100];//최대 동물 100마리 저장 가능
+	class animal* animal_list[100];//동적 할당 (최대 동물 100마리 저장 가능)
 	int size = 0;//동물 개수
 
 public:
-	void save(char* name, int year, char* species) {
-		animal_list[size] = new animal(name, year, species);
+	void save(char* name, int year, char* species) {//저장 함수
+		animal_list[size] = new animal(name, year, species);//개체 별 동적할당
 		size++;
 	}
-
-
-
-
 	void size_setter(int size) { this->size = size; }
-	void size_getter() { this->size; }
-	void animal_setter() {
-		size_getter();
+	int size_getter() { return this->size; }
+	void animal_getter(int size) {
 		this->animal_list[size];
 	}
-	//zoo(animal_list[]);
-
-	
+	void print(int i);
+	//zoo(animal_list[]);	
 };
 
-animal::animal(char* name, int year, char* species) {
+animal::animal(char* name, int year, char* species) {//동물 저장
 	strcpy(this->name, name);
 	this->year = year;
 	strcpy(this->species, species);
-	
 }
 
-
+void zoo::print(int i) {
+	cout << "----------" << i << "----------" << endl;
+	animal_getter(size);
+	cout << "Name : " << animal_list[i]->get_name() << endl;
+	cout << "Year : " << animal_list[i]->get_year() << endl;
+	cout << "Species : " << animal_list[i]->get_species() << endl;
+	cout << "---------------------" << endl;
+}
 
 int main() {
-	zoo* Zoo = new zoo;
+	zoo* Zoo = new zoo;//zoo 객체 선언
 	char input[100]={};
 	char name_i[10];
 	int year_i;
 	char species_i[100];
+	int size;
 
 	while (1) {
 		cout << "Please Enter Command(new_animal,print_all,print_species,exit) :";
@@ -60,12 +63,16 @@ int main() {
 
 		if (strcmp(input, "new_animal") == 0) {
 			cin >> name_i >> year_i >> species_i;
-
-			
+			Zoo->save(name_i, year_i, species_i);
 		}
 
 		else if (strcmp(input, "print_all") == 0) {
-			//d
+			size=Zoo->size_getter();
+			cout << "------print_all------" << endl;
+			for (int i = 0; i < size; i++) {
+				Zoo->print(i);
+			}
+			cout << "---------------------" << endl;
 		}
 
 		else if (strcmp(input, "print_species") == 0) {
@@ -82,6 +89,5 @@ int main() {
 	//동적 할당 해제
 	return 0;
 }
-
 
 //zoo->animal->name,year,species
