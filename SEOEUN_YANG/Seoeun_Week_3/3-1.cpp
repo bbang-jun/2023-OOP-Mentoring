2a92b63bde712e8d34c2a502e7bce7856f2ee056
SEOEUN YANG <03ann@naver.com>
2023-02-09 13:33:48 +0900

    fix : 3-1 DELETE 구현

--- SEOEUN_YANG/Seoeun_Week_3/3-1.cpp
+++ SEOEUN_YANG/Seoeun_Week_3/3-1.cpp
@@ -6,7 +6,8 @@ class Node {//노드 클래스 생성
 private:
 	int ID;
 	char name[100];
-	Node* next;  // 다음 노드를 가리키는 포인터
+	Node* prev;//이전 노드 가리킴
+	Node* next;//다음 노드 가리킴
 public:
 	Node() {//생성자
 		ID = 0;//초기화
@@ -44,6 +45,7 @@ public:
 	}
 	void INSERT(int ID,char* name);
 	void PRINT();
+	void PRINT_REV();
 	void FIND(int value);
 	void DELETE(int value);
 };
@@ -75,6 +77,50 @@ void List::PRINT() {
 	}
 }
 
+void List::DELETE(int ID) {
+	Node* curNode = head;
+	//변수 느낌
+	Node* prevNode = NULL;
+	Node* delNode = NULL;
+
+	while (curNode != NULL) {//노드에 데이터가 있을 때
+		if (curNode->getID() == ID)//지우고 싶은 값일 때 while문 탈출
+			break;
+		prevNode = curNode;//지우고 싶은 값이 아니면 노드 이동 (prev에 현재 노드 저장, 포인터 옮길 때 이용)
+		curNode = curNode->getNext();
+	}
+
+	//while문 탈출 이후
+	if (curNode == head) { // 1. head를 삭제하는 경우
+		if (head->getNext() == NULL) { // list에 head만 존재하는 경우
+			delNode = head;
+			delete head;
+			// head = NULL; 팁1
+
+			this->size--;//-> size=0
+		}
+		else { // list에 head 외에 다른 노드들도 존재하는 경우
+			delNode = head;
+			head = head->getNext();//head 다음 노드를 head로 설정
+			delete delNode;
+			this->size--;
+		}
+	}
+	else if (curNode == tail) { // 3. tail을 삭제하는 경우
+		delNode = tail;
+		tail = prevNode;//tail 전 노드 tail로 설정
+		delete delNode;
+		tail->setNext(NULL); // 팁2 //쓰레기값 방지 (tail 항상 NULL 가리킴)
+		this->size--;
+	}
+	else { // 2. head와 tail을 제외한 다른 노드들을 제거하는 경우
+		delNode = curNode;
+		prevNode->setNext(curNode->getNext());//cur노드 전의 노드를 cur노드 이후의 노드를 가리키게 만듦
+		delete delNode;//cur노드 삭제
+		this->size--;
+	}
+}
+
 int main(void) {
 	int command;
 	int ID = 0;
@@ -96,8 +142,7 @@ int main(void) {
 		}
 
 		else if (command == 3) {
-			//cin >> num;
-			//valueList->DELETE(num);
+			valueList->PRINT_REV();
 		}
 
 		else if (command == 4) {
