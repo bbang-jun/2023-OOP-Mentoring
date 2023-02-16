b910c21b7922333bb3b8080ac806bb35fc67d642
moonuckjoo <dnjswn1756@naver.com>
2023-02-17 00:05:23 +0900

    Feat: print

--- Wonju/Wonju/Wonju/4-1.cpp
+++ Wonju/Wonju/Wonju/4-1.cpp
@@ -36,12 +36,13 @@ private:
 	int size;
 	Node* root;
 	Node* Parent;
-
+	Node* curNode;
 public:
 	Tree() {
 		size = 0;
 		root = NULL;
 		Parent = NULL;
+		curNode = NULL;
 	}
 	~Tree() {
 		allocate(root);
@@ -80,7 +81,7 @@ void Tree::INSERT(Node* curNode, int integer) { // 루트 노드가 없는 경우
 	}
 	else if (curNode->getValue() < integer) // 현재 노드에 저장된 값보다 큰 경우
 	{
-		if (curNode->getRightChild() == NULL) {
+		if (curNode->getRightChild() == NULL) { //오른쪽 자식이 있는 경우
 			Node* newNode = new Node;
 			newNode->setValue(integer);
 			curNode->setRightChild(newNode);
@@ -93,6 +94,77 @@ void Tree::INSERT(Node* curNode, int integer) { // 루트 노드가 없는 경우
 		}
 	}
 }
+void Tree::PrintPreorder(Node* curNode) { // 전위순회 출좌우
+	if (curNode == nullptr)
+		return;
+	cout << curNode->getValue() << " ";
+	PrintPreorder(curNode->getLeftChild()); 
+	PrintPreorder(curNode->getRightChild());
+}
+
+void Tree::PrintInorder(Node* curNode) { // 중위순회 좌출우
+	if (curNode == nullptr)
+		return;
+	PrintInorder(curNode->getLeftChild());
+	cout << curNode->getValue() << " ";
+	PrintInorder(curNode->getRightChild());
+}
+
+void Tree::PrintPostorder(Node* curNode) { // 후위순회 좌우출
+	if (curNode == nullptr)
+		return;
+	PrintPostorder(curNode->getLeftChild());
+	PrintPostorder(curNode->getRightChild());
+	cout << curNode->getValue() << " ";
+}
+
 int main(void) {
-	return 0;
+	Tree* tree = new Tree;
+	int Command = 0;
+	int integer = 0;
+
+	while (1)
+	{
+		cout << "Enter Any Command(1: Insert, 2:Delete, 3:Find, 4:Preorder, 5:Inorder, 6:Postorder, 7:Leaf node, 8:Exit) : ";
+		cin >> Command;
+		if (Command == 1)
+		{
+			cin >> integer;
+			tree->INSERT(tree->getRoot(), integer);
+		}
+		else if (Command == 2)
+		{
+
+		}
+		else if (Command == 3)
+		{
+
+		}
+		else if (Command == 4)
+		{
+			tree->PrintPreorder(tree->getRoot());
+			cout << endl;
+		}
+		else if (Command == 5)
+		{
+			tree->PrintInorder(tree->getRoot());
+			cout << endl;
+		}
+		else if (Command == 6)
+		{
+			tree->PrintPostorder(tree->getRoot());
+			cout << endl;
+		}
+		else if (Command == 7)
+		{
+
+		}
+		else if (Command == 8)
+		{
+			break;
+		}
+		else
+			cout << "보기 내에서 숫자를 선택해주세요";
+	}
+	
 }
